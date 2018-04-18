7a01fb8b3e5f25971bd25d24a4d52e0ee36c4339
Daniel <dfritsch99@gmail.com>
2018-04-18 01:55:02 -0700

    Done for the night. Added more style and editted the work/projects cards

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
   return render_template ("index.html")
 
-@app.route("/projects")
-def projects():
-  return render_template ('projects.html')
-
-@app.route("/work")
-def work():
-  return render_template ('work.html')
-
-@app.route("/education")
-def education():
-  return render_template ('education.html')
-
 @app.route("/resume")
 def resume():
   return render_template ('file:///Users/danielfritsch/Desktop/Daniel_Fritsch_Resume.pdf')
-
-@app.route("/contact")
-def contact():
-  return render_template ('contact.html')
 
 if __name__ == "__main__":
   app.run()
